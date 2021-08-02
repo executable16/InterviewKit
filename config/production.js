c699c03c43f1ee4bfcfae6ce5767fc36a53bacbe
executable16 <atulmalakar15@gmail.com>
2021-08-03 02:07:47 +0530

    Intial commit @ Heroku

---
+++ config/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+    GoogleClientId : '854778099826-2mf7ik8vumpe9pqtq2966lfteqb3d7ej.apps.googleusercontent.com',
+    GoogleClientSecret : '_MoZdMx_OUhcx0EDpxwRSXwx',
+    MongoURI : 'mongodb+srv://AtulMalakar:baBmAkQbNj8YFpM@interviewkit.hrgga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey : 'jkdkd&37y73939890hdhndjjij8dijakjdkaatula45444'
+}
