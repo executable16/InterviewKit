ad01794d00c13be626b5f8b92a17ac59467189f8
executable16 <atulmalakar15@gmail.com>
2021-08-03 02:31:00 +0530

    Initial commit @ Heroku

--- config/production.js
+++ config/production.js
@@ -1,4 +1,12 @@
 module.exports = {
+    GoogleClientId : process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    MongoURI : process.env.MONGO_URI,
+    cookieKey : process.env.COOKIE_KEY
+}
+
+
+module.exports = {
     GoogleClientId : '854778099826-2mf7ik8vumpe9pqtq2966lfteqb3d7ej.apps.googleusercontent.com',
     GoogleClientSecret : '_MoZdMx_OUhcx0EDpxwRSXwx',
     MongoURI : 'mongodb+srv://AtulMalakar:baBmAkQbNj8YFpM@interviewkit.hrgga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
