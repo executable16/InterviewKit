53bac47495bc7c91281b6324f431708d0b3a6e08
Atul Malakar <atulmalakar15@gmail.com>
2021-09-14 23:56:56 +0530

    Update production.js

--- config/production.js
+++ config/production.js
@@ -4,11 +4,3 @@ module.exports = {
     MongoURI : process.env.MONGO_URI,
     cookieKey : process.env.COOKIE_KEY
 }
-
-
-module.exports = {
-    GoogleClientId : '854778099826-2mf7ik8vumpe9pqtq2966lfteqb3d7ej.apps.googleusercontent.com',
-    GoogleClientSecret : '_MoZdMx_OUhcx0EDpxwRSXwx',
-    MongoURI : 'mongodb+srv://AtulMalakar:baBmAkQbNj8YFpM@interviewkit.hrgga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    cookieKey : 'jkdkd&37y73939890hdhndjjij8dijakjdkaatula45444'
-}
